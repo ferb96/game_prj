a1988fad0cdf2c91e322760c2c23e8fca42be5c8
ferb96 <fduy96@gmail.com>
2015-01-20 11:10:40 -0600

    Implemented main.cc

--- main.cc
+++ main.cc
@@ -1,4 +1,17 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+#include "SDL2/SDL2_gfxPrimitives.h"
+#include "game.h"
 
+// Size of the window
+const int WINDOW_SIZE_X = 800;
+const int WINDOW_SIZE_Y = 600;
+
+// THE GLORIOUS MAIN FUNCTION
 int main()
-	runGame();
+	Game theGame;
+	if ( !theGame.init(WINDOW_SIZE_X, WINDOW_SIZE_Y) );
+		return -1;
+
+	theGame.runGame()
 	return 0;
