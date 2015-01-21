e0accdebfaf0813a6c2f87d8a97a052e20e8c106
jpobloc2 <jpobloc2@gustavus.edu>
2015-01-21 11:39:54 -0600

    Testing player

--- bullet.h
+++ bullet.h
@@ -1,8 +1,14 @@
+#include <SDL2/SDL.h>
+#include "object.cc"
 
+#ifndef BULLET_H
+#define BULLET_H
 
 class Bullet: public Object
 {
 public:
-	Bullet(int x_pos, int y_pos, double traj)
-	void drawSelf(SDL_Renderer* rend, SDL_Surface* surf);
-}
+	Bullet(int x_pos, int y_pos, double traj);
+	void drawSelf(SDL_Renderer* rend);
+};
+
+#endif /* BULLET_H */
