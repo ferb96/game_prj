86f96e3031a4089f5701a6dff389d7565b21fb20
ferb96 <fduy96@gmail.com>
2015-01-21 10:14:13 -0600

    modified Game class further, not completed yet, just saving changes

--- game.h
+++ game.h
@@ -1,25 +1,62 @@
-Class Game
+#ifndef GAME_H
+#define GAME_H
+
+#include <SDL2/SDL.h>
+#include "SDL2/SDL2_gfxPrimitives.h"
+#include "asteroid.h"
+#include "bullet.h"
+#include "player.h"
+#include <vector>
+
+class Game
 {
 public:
-	Game(int winSizeX, int WinSizeY);
+	Game();
 	bool init();
 	void runGame(); 
+
 private:
+	//enumerators
+	enum playerStates { 
+		MOVING_UP,
+		MOVING_DOWN,
+		MOVING_LEFT,
+		MOVING_RIGHT,
+		SHOOTING,
+		TOTAL
+	};
+
+	//gameplay-related values
+	int asteroidSplitNumber;
+	int delayBetweenBullets;
+	int playerMoveSpeed;
+	int asteroidMoveSpeed;
+	int bulletMoveSpeed;
+	int winSizeX;
+	int winSizeY;
+
+	//player actions: array of boolean
+	bool playerAction[TOTAL];
+
 	//declare vector of asteroids here (not sure how to works with pointers yet so just leave it here)
 	//declare vector of bullets here
 	//declare player object here
+
+	//SDL elements
 	SDL_Window* window;
 	SDL_Renderer* renderer;
 
 	//init functions
-	bool InitSDL();
+	bool initSDL();
 	bool createWindow();
 	bool createRenderer();
-	bool setup Renderer();
+	void setupRenderer();
 
 	//processing functions
 	void processInput();
 	void moveObjects();
 	void renderObjects();
 	void close();
-}
+};
+
+#endif /* GAME_H */
--- main.cc
+++ main.cc
@@ -3,14 +3,10 @@
 #include "SDL2/SDL2_gfxPrimitives.h"
 #include "game.h"
 
-// Size of the window
-const int WINDOW_SIZE_X = 800;
-const int WINDOW_SIZE_Y = 600;
-
 // THE GLORIOUS MAIN FUNCTION
 int main()
 	Game theGame;
-	if ( !theGame.init(WINDOW_SIZE_X, WINDOW_SIZE_Y) );
+	if ( !theGame.init() );
 		return -1;
 
 	theGame.runGame()
