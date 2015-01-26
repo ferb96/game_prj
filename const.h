70b4ecf9cdfc020cdc95cc0408bac8e46cab0fe9
ferb96 <fduy96@gmail.com>
2015-01-26 14:11:05 -0600

    Added const.h to manage global constants

---
+++ const.h
@@ -0,0 +1,8 @@
+const int WINDOW_SIZE_X = 800;
+const int WINDOW_SIZE_Y = 600;
+const int ASTEROID_SPLIT_NUMBER = 2;
+const int DELAY_BETWEEN_BULLETS = 250; 
+const int BULLET_LIFESPAN = 500;
+const int PLAYER_MOVE_SPEED = 8; //not working
+const int BULLET_MOVE_SPEED = 15; 
+const double MAX_SPD = 1.5;
